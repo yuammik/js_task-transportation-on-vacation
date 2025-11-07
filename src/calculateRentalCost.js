392305c865825f305e4be0495d398f445da7746e
Iryna Hrytsak <homekgamek@gmail.com>
2025-11-07 23:19:34 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,17 @@
  */
 function calculateRentalCost(days) {
   let result = days * 40;
+  let discount = 0;
 
-  if (days > 6) {
-    result -= 50;
-  } else if (days > 2) {
-    result -= 20;
+  if (days > 2) {
+    discount = 20;
   }
+
+  if (days >= 7) {
+    discount = 50;
+  }
+
+  result -= discount;
 
   return result;
 }
